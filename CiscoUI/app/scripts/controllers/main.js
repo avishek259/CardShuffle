f3423c8be43f5c324fba7930240e1e96c836edea
Avishek Chakraborty <avishek.chakraborty@emirates.com>
2017-07-08 23:54:31 +0400

    Client side

---
+++ CiscoUI/app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shippingFormApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the shippingFormApp
+ */
+angular.module('shippingFormApp')
+  .controller('MainCtrl', function () {
+    //TODO
+  });
